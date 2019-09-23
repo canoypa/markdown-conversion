dc94190bd41ef03b29548ec02b227a053987898b
cano <mahorowa3088@gmail.com>
2019-09-23 19:30:23 +0900

    モジュールを es2015 に変更

--- docs/markdown-conversion.js
+++ docs/markdown-conversion.js
@@ -1,7 +1,5 @@
-"use strict";
-exports.__esModule = true;
 var conversion = function (text) {
     var result = document.createDocumentFragment();
     return result;
 };
-exports["default"] = conversion;
+export default conversion;
