d9f45006d0092eb9236ad84726659b37e1f8e677
Cano <developer.sasazono@gmail.com>
2019-09-23 20:47:08 +0900

    h1 の変換とか

--- src/markdown-conversion.ts
+++ src/markdown-conversion.ts
@@ -1,5 +1,21 @@
 const conversion = (text: string): DocumentFragment => {
-  const result = document.createDocumentFragment();
+  const result: DocumentFragment = document.createDocumentFragment();
+  const lineArray: string[] = text.split(/\n/);
+
+  lineArray.forEach((line: string) => {
+    if (/^# (.+)/.test(line)) {
+      const h1Match = <RegExpExecArray>/^# (.+)/.exec(line);
+
+      const h1 = document.createElement('h1');
+      h1.classList.add('markdown-h1');
+      h1.textContent = h1Match[1];
+      result.appendChild(h1);
+    } else {
+      const div = document.createElement('div');
+      div.textContent = line;
+      result.appendChild(div);
+    }
+  });
 
   return result;
 };
