13cc79a167595f49588faefd5f45cbcb19cd5e28
Cano <developer.sasazono@gmail.com>
2019-09-24 21:15:05 +0900

    🚧 conversion 関数を conversion クラスに追加

--- src/markdown-conversion.ts
+++ src/markdown-conversion.ts
@@ -2,6 +2,30 @@ const RegExpHeading: RegExp = new RegExp(/^(#+) (.+)/);
 
 class _conversion {
   constructor() {}
+  change(text: string): DocumentFragment {
+    const result: DocumentFragment = document.createDocumentFragment();
+    const lineArray: string[] = text.split(/\n/);
+
+    lineArray.forEach((line: string) => {
+      if (RegExpHeading.test(line)) {
+        const match = <RegExpExecArray>RegExpHeading.exec(line);
+        const stage: number = match[1].length;
+
+        const heading = document.createElement(`h${stage}`);
+        heading.id = `m-${match[2]}`;
+        heading.classList.add(`markdown-h${stage}`);
+        heading.textContent = match[2];
+        result.appendChild(heading);
+      } else {
+        const p = document.createElement('p');
+        p.classList.add('markdown-body-text');
+        p.textContent = line;
+        result.appendChild(p);
+      }
+    });
+
+    return result;
+  }
 }
 
 const conversion = (text: string): DocumentFragment => {
