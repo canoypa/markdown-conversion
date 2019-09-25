15e9a046dfd4c653fe83713a399e0f272dd0b6af
cano <mahorowa3088@gmail.com>
2019-09-25 22:07:11 +0900

    🚧 登録された関数での処理に変更

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,17 @@ const textarea: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementBy
 const output: HTMLOutputElement = <HTMLOutputElement>document.getElementById('output');
 const conversion: Conversion = new Conversion();
 
+conversion.register(/^(#{1,6})\s(.+)$/gm, (exec: RegExpExecArray) => {
+  const stage: number = exec[1].length;
+  const heading = document.createElement(`h${stage}`);
+
+  heading.id = `m-${exec[2]}`;
+  heading.classList.add(`markdown-h${stage}`);
+  heading.textContent = exec[2];
+
+  return heading;
+});
+
 textarea.addEventListener('input', textareaChange);
 
 textareaChange(); //debug
--- src/markdown-conversion.ts
+++ src/markdown-conversion.ts
@@ -1,5 +1,3 @@
-const RegExpHeading: RegExp = new RegExp(/^(#+) (.+)/);
-
 class Conversion {
   private process: Array<{ regexp: RegExp; callback: Function }> = [];
 
@@ -7,23 +5,16 @@ class Conversion {
 
   change(text: string): DocumentFragment {
     const result: DocumentFragment = document.createDocumentFragment();
-    const lineArray: string[] = text.split(/\n/);
 
-    lineArray.forEach((line: string) => {
-      if (RegExpHeading.test(line)) {
-        const match = <RegExpExecArray>RegExpHeading.exec(line);
-        const stage: number = match[1].length;
+    this.process.forEach(process => {
+      if (process.regexp.test(text)) {
+        process.regexp.lastIndex = 0;
 
-        const heading = document.createElement(`h${stage}`);
-        heading.id = `m-${match[2]}`;
-        heading.classList.add(`markdown-h${stage}`);
-        heading.textContent = match[2];
-        result.appendChild(heading);
-      } else {
-        const p = document.createElement('p');
-        p.classList.add('markdown-body-text');
-        p.textContent = line;
-        result.appendChild(p);
+        let exec: RegExpExecArray | null = null;
+        while ((exec = process.regexp.exec(text))) {
+          console.log(exec);
+          result.appendChild(process.callback(exec));
+        }
       }
     });
 
